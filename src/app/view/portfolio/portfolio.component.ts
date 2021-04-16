49eb69f78ce32a447bbd9d42bffaef375cca8c6b
Shtempler Viktor <eviljosephe@gmail.com>
2021-04-16 17:59:54 +0300

    Profile Complete

--- src/app/view/portfolio/portfolio.component.ts
+++ src/app/view/portfolio/portfolio.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {Images}            from '../../data/urlImages/urlImages';
+import {Component, OnInit}           from '@angular/core';
+import {Images, PortfolioIconImages} from '../../data/urlImages/urlImages';
 
 @Component({
              selector   : 'app-portfolio',
@@ -8,12 +8,35 @@ import {Images}            from '../../data/urlImages/urlImages';
            })
 export class PortfolioComponent implements OnInit {
   iconBack = '../../../../assets/svg/4_Icon.svg';
+  test = '../../../../assets/img/banners/banners_1.jpg';
   URLImages = Images;
+  urlIconsProfile= PortfolioIconImages
+  categories = Object.keys(this.URLImages);
+  category;
+  viewBigWindow = `hidden`;
+  bigContent;
 
   constructor() {
   }
 
   ngOnInit(): void {
+    console.log(Object.keys(this.URLImages).length);
   }
 
+
+  selectCategory(categorySelected): void {
+    this.category = this.URLImages[categorySelected];
+    console.log(this.category);
+
+  }
+
+  contentClicked(content) {
+    this.viewBigWindow = 'visible';
+    this.bigContent=content;
+    console.log(this.bigContent)
+  }
+
+  closeBigWindow(){
+    this.viewBigWindow='hidden';
+  }
 }
